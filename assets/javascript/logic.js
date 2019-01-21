56365ebb9d2b73b8371962a46a85dd4495637672
crsimpson5 <crsimpson5@gmail.com>
2019-01-20 19:34:06 -0600

    added styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,16 +17,24 @@ var destination = "";
 var firstTime = "";
 var frequency = 0;
 
+// Submit button click handler
 $("#submit-btn").on("click", function (event) {
   event.preventDefault();
 
+  // Get data from form
   name = $("#name-input").val().trim();
   destination = $("#dest-input").val().trim();
   firstTime = $("#time-input").val().trim();
   frequency = parseInt($("#freq-input").val().trim());
 
+  // If input is invalid don't submit and display modal
   if (name === "" || destination === "" ||
-    firstTime === "" || frequency === "") return;
+      firstTime === "" || frequency === "" ||
+      moment(firstTime, "HH:mm", true).isValid() === false ||
+      isNaN(frequency) || frequency <= 0) {
+    $("#error-modal").modal();
+    return;
+  }
 
   $("#name-input").val("");
   $("#dest-input").val("");
@@ -42,6 +50,21 @@ $("#submit-btn").on("click", function (event) {
   });
 });
 
+// Trash can icon click handler
+$(document.body).on("click", "#trash-can", function() {
+
+  var key = $(this).attr("data-key");
+  console.log("key: " + key);
+
+  // Remove from database
+  database.ref().child(key).remove();
+
+  // Remove row from table
+  console.log($(this).closest("tr"));
+  $(this).closest("tr").remove();
+});
+
+// Initial load and listener for database
 database.ref().orderByChild("dateAdded").on("child_added", function (childSnap) {
 
   console.log(childSnap.val().name);
@@ -49,6 +72,7 @@ database.ref().orderByChild("dateAdded").on("child_added", function (childSnap) 
   console.log(childSnap.val().firstTime);
   console.log(childSnap.val().frequency);
 
+  // Get the time for next train and how many minutes away it is
   var firstTimeConverted = moment(childSnap.val().firstTime, "HH:mm").subtract(1, "years");
   console.log(firstTimeConverted);
 
@@ -56,7 +80,7 @@ database.ref().orderByChild("dateAdded").on("child_added", function (childSnap) 
   console.log("DIFFERENCE IN TIME: " + diffTime);
 
   var tRemainder = diffTime % childSnap.val().frequency;
-  console.log(tRemainder);
+  console.log("TIME REMAINING: " + tRemainder);
 
   var tNextTrain = childSnap.val().frequency - tRemainder;
   console.log("MINUTES TILL TRAIN: " + tNextTrain);
@@ -66,12 +90,14 @@ database.ref().orderByChild("dateAdded").on("child_added", function (childSnap) 
 
   var newRow = $("<tr>");
 
+  // Create new table row
   newRow.append([
     $("<td>").text(childSnap.val().name),
     $("<td>").text(childSnap.val().destination),
     $("<td>").text(childSnap.val().frequency),
     $("<td>").text(nextTrain),
-    $("<td>").text(tNextTrain)
+    $("<td>").text(tNextTrain),
+    $("<td>").html("<i class='far fa-trash-alt' id='trash-can' data-key=" + childSnap.key + "></i>")
   ]);
 
   $("#schedule-table").append(newRow);
